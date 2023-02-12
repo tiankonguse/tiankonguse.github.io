82ea5cda347bfc702fdfb5231a93e13aaf583298
tiankonguse <i@tiankonguse.com>
2023-02-12 12:23:11 +0800

    add

--- stuff/rummikub/index.js
+++ stuff/rummikub/index.js
@@ -1,65 +1,70 @@
 const colors = { "red": "红", "blue": "蓝", "pink": "粉", "black": "黑" };
-function Init(globalData) {
+var globalData = {};
+function Init(data) {
+    globalData = data;
 
     for (var color in colors) {
         globalData[color] = [];
         globalData[color].push(0);
 
-        var row = jQuery('.' + color)
-        row.append('<div class="col num select-num"> <span class="val"> ' + colors[color] + '</span> </div>');
+        var row1 = jQuery('.row.one.' + color)
+        var row2 = jQuery('.row.two.' + color)
+        row1.append('<div class="col num one select-num col-head"> <span class="val">' + colors[color] + '</span> </div>');
+        row2.append('<div class="col num two select-num col-head"> <span class="val">' + colors[color] + '</span> </div>');
         for (var i = 1; i <= 13; i++) {
             globalData[color].push(0);
-            row.append('<div class="col num"> <span class="val">' + i + '</span> </div>');
-            row.append('<div class="col num"> <span class="val">' + i + '</span> </div>');
+            row1.append('<div class="col num one btn" attr-val="' + i + '" attr-color="' + color + '"> <span class="val">' + i + '</span> </div>');
+            row2.append('<div class="col num two btn" attr-val="' + i + '" attr-color="' + color + '"> <span class="val">' + i + '</span> </div>');
         }
     }
 }
 
-function render(globalData) {
+function render() {
     for (var color in colors) {
-        var $cols = jQuery("." + color + " .num");
-        for (var i = 1; i <= 26; i += 2) {
-            var offset = parseInt((i + 1) / 2);
+        var $cols1 = jQuery("." + color + " .num .one");
+        var $cols2 = jQuery("." + color + " .num .two");
+        for (var i = 1; i <= 13; i++) {
+            var offset = i;
             var val = globalData[color][offset];
             if (val == 0) {
-                jQuery($cols[i]).removeClass('select-num');
-                jQuery($cols[i + 1]).removeClass('select-num');
+                jQuery($cols1[offset]).removeClass('select-num');
+                jQuery($cols2[offset]).removeClass('select-num');
             } else if (val == 1) {
-                jQuery($cols[i]).addClass('select-num');
-                jQuery($cols[i + 1]).removeClass('select-num');
+                jQuery($cols1[offset]).addClass('select-num');
+                jQuery($cols2[offset]).removeClass('select-num');
             } else {
-                jQuery($cols[i]).addClass('select-num');
-                jQuery($cols[i + 1]).addClass('select-num');
+                jQuery($cols1[offset]).addClass('select-num');
+                jQuery($cols2[offset]).addClass('select-num');
             }
         }
     }
 
 }
 
-function ClickAdd(globalData) {
-    var color = jQuery("#selec-control-color").val();
-    var num = parseInt(jQuery("#selec-control-num").val());
+function Click($num) {
+    if ($num.hasClass('col-head')) return;
+    var color = $num.attr('attr-color');
+    var num = parseInt($num.attr('attr-val'));
+    if ($num.hasClass('select-num')) {
+        if (globalData[color][num] == 0) {
+            alert("删除错误：" + color + " " + num + "为空")
+            return;
+        }
 
-    if (globalData[color][num] == 2) {
-        alert("增加错误：" + color + " " + num + "已经有两个了")
-        return;
+        globalData[color][num]--;
+        $num.removeClass('select-num');
+    } else {
+        if (globalData[color][num] == 2) {
+            alert("增加错误：" + color + " " + num + "已经有两个了")
+            return;
+        }
+        globalData[color][num]++;
+        $num.addClass('select-num');
     }
-    globalData[color][num]++;
-    render(globalData);
+    console.log(globalData);
+
 }
 
-function ClickDel(globalData) {
-    var color = jQuery("#selec-control-color").val();
-    var num = parseInt(jQuery("#selec-control-num").val());
+function Check() {
 
-    if (globalData[color][num] == 0) {
-        alert("删除错误：" + color + " " + num + "为空")
-        return;
-    }
-    globalData[color][num]--;
-    render(globalData);
-}
-
-function Check(globalData) {
-    
 }
