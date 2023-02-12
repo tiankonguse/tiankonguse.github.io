2c541e914725de2f8198b771ed8ae704922fe379
tiankonguse <i@tiankonguse.com>
2023-02-12 19:13:51 +0800

    add

--- stuff/rummikub/index.js
+++ stuff/rummikub/index.js
@@ -7,14 +7,12 @@ var dpData = {};
 const K = 13;
 const KK = 26;
 const BIT = 2;
-const selectCase = [[], [0, 1, 2], [0, 1, 2, 3], [0, 1, 2, 3, 4]];
+const selectCase = [3, 4, 5];
 
-function CheckNums(num, nums, counts) {
-    for (var k in counts) {
-        var kk = parseInt(k);
-        if (counts[kk] == 0) continue;
-        if (num + kk >= K) return false;
-        if (nums[num + kk] + counts[kk] > BIT) {
+function CheckNums(num, nums, n) {
+    if (num + n > K) return false;
+    for (var k = 0; k < n; k++) {
+        if (nums[num + k] + 1 > BIT) {
             return false;
         }
     }
@@ -34,19 +32,18 @@ function CalMask(nums) {
     return mask;
 }
 
-function BornAns(num, nums) {
+function BornAns(num, n) {
     var ans = [];
-    for (var i in nums) {
-        ans.push(num + parseInt(i));
+    for (var i = 0; i < n; i++) {
+        ans.push(num + i);
     }
     return ans
 }
 
 var maskNum = 0;
-function AddAns(mask, num, i, maskTmp) {
+function AddAns(mask, num, n, maskTmp) {
     dpData[maskTmp] = {
-        "flag": 1,
-        "ans": BornAns(num, selectCase[i]),
+        "ans": BornAns(num, n),
         "pre": mask
     };
     maskNum = maskNum + 1;
@@ -70,7 +67,6 @@ function MaskToNums(mask) {
 
 function InitDp() {
     dpData[0] = {
-        "flag": 1,
         "ans": [],
         "pre": -1
     }
@@ -89,30 +85,27 @@ function InitDp() {
         // console.log("mask", mask, nums);
 
         for (var num = 0; num < K; num++) {
-            for (var i = 1; i < selectCase.length; i++) {
-                var counts = [0, 0, 0, 0, 0];
-                for (var k in selectCase[i]) {
-                    counts[parseInt(k)]++;
-                }
-                if (!CheckNums(num, nums, counts)) {
-                    // console.log("ckeck false mask=", mask, "num=", num, "i=", i, "j=", j, nums, counts);
+            for (var i = 0; i < selectCase.length; i++) {
+                const n = selectCase[i];
+                if (!CheckNums(num, nums, n)) {
+                    
                     continue;
                 }
 
-                for (var k in counts) {
-                    nums[num + parseInt(k)] += counts[k];
+                for (var k = 0; k < n; k++) {
+                    nums[num + k]++;
                 }
                 var maskTmp = CalMask(nums);
 
                 if (!dpData.hasOwnProperty(maskTmp)) {
-                    AddAns(mask, num, i, maskTmp);
+                    AddAns(mask, num, n, maskTmp);
                     // console.log("mask=", mask, "num=", num, "i=", i, "j=", j, "maskTmp=", maskTmp, nums);
                     que.push(maskTmp);
                 }
 
 
-                for (var k in counts) {
-                    nums[num + parseInt(k)] -= counts[k];
+                for (var k = 0; k < n; k++) {
+                    nums[num + k]--;
                 }
 
             }
@@ -128,7 +121,6 @@ function Init(data) {
 
     for (var color in colors) {
         globalData[color] = [];
-        globalData[color].push(0);
 
         var row1 = jQuery('.card-num .row.one.' + color)
         var row2 = jQuery('.card-num .row.two.' + color)
@@ -150,19 +142,19 @@ function Click($num) {
     var color = $num.attr('attr-color');
     var num = parseInt($num.attr('attr-val'));
     if ($num.hasClass('select-num')) {
-        if (globalData[color][num] == 0) {
+        if (globalData[color][num - 1] == 0) {
             alert("删除错误：" + color + " " + num + "为空")
             return;
         }
 
-        globalData[color][num]--;
+        globalData[color][num - 1]--;
         $num.removeClass('select-num');
     } else {
-        if (globalData[color][num] == 2) {
+        if (globalData[color][num - 1] == 2) {
             alert("增加错误：" + color + " " + num + "已经有两个了")
             return;
         }
-        globalData[color][num]++;
+        globalData[color][num - 1]++;
         $num.addClass('select-num');
     }
 }
@@ -253,63 +245,174 @@ function DfsCheck(num, colorNums, ans) {
     return false;
 }
 
-function renderAns(ans) {
-    $(".card-ans .row").remove();
-    $card = $(".card-ans");
-    var gData = []
-    for (var k in ans) {
-        var d = ans[k];
-        var kk = parseInt(k);
-        var x = 10;
-        var oneWidth = 80
-        y += 100;
-
-        var pData = {
-            "childs": []
-        }
-        gData.push(pData);
-
-        if (d.type == "num") {
-            var num = d.num;
-            var $row = jQuery('<div class="row"></div>');
-            for (var j in d.value) {
-                if (d.value[j] == 0) continue;
-                $row.append('<div class="col num select-num col-head ' + colorName[j] + '"> <span class="val">' + num + '</span> </div>');
-
-                pData.childs.push({
-                    "num": num,
-                    "color": colorName[j]
-                });
+function InitMobile(height) {
+    globalData.width = $(window).width() * 5 / 6;
+    globalData.graphF6 = new F6.Graph({
+        container: document.getElementById('container-f6'),
+        grid: true,
+        width: globalData.width,
+        height: height,
+        fitView: true,
+        fitViewPadding: 60,
+        // Set groupByTypes to false to get rendering result with reasonable visual zIndex for combos
+        groupByTypes: false,
+        defaultCombo: {
+            type: 'rect',
+            size: [100, 10], // The minimum size of the Combo
+            style: {
+                lineWidth: 1,
             }
-            $card.append($row);
-        } else {
-            var $row = jQuery('<div class="row"></div>');
-            for (var j in d.value) {
-                var num = d.value[j];
-                $row.append('<div class="col num select-num col-head ' + d.color + '"> <span class="val">' + num + '</span> </div>');
+        },
+        defaultNode: {
+            type: 'rect',
+            size: [20, 30], // The minimum size of the Co
+            labelCfg: {
+                style: {
+                    fill: 'white',
+                    fontSize: 14,
+                },
+            },
+        },
+        modes: {
+            default: [
+                'drag-canvas',
+                'drag-node',
+                'drag-combo',
+                'collapse-expand-combo',
+                'click-select',
+            ],
+        },
+    });
+    globalData.graphF6.render();
+    globalData.graphF6.fitView();
 
-                pData.childs.push({
-                    "num": num,
-                    "color": d.color
-                });
-            }
-            $card.append($row);
+    // 监听
+    globalData.graphF6.on('combo:dragend', () => {
+        globalData.graphF6.getCombos().forEach((combo) => {
+            globalData.graphF6.setItemState(combo, 'dragenter', false);
+        });
+    });
+    globalData.graphF6.on('node:dragend', () => {
+        globalData.graphF6.getCombos().forEach((combo) => {
+            globalData.graphF6.setItemState(combo, 'dragenter', false);
+        });
+    });
 
-        }
-    }
+    globalData.graphF6.on('combo:dragenter', (e) => {
+        globalData.graphF6.setItemState(e.item, 'dragenter', true);
+    });
+    globalData.graphF6.on('combo:dragleave', (e) => {
+        globalData.graphF6.setItemState(e.item, 'dragenter', false);
+    });
 
-    globalData.graph.clearCells();
+    globalData.graphF6.on('combo:mouseenter', (evt) => {
+        const item = evt.item;
+        globalData.graphF6.setItemState(item, 'active', true);
+    });
 
+    globalData.graphF6.on('combo:mouseleave', (evt) => {
+        const item = evt.item;
+        globalData.graphF6.setItemState(item, 'active', false);
+    });
+}
+
+function renderMobie(gData) {
     var y = 0;
     var cellWidth = globalData.width / 10;
     var cellHeight = 60;
     var cellFontSize = 25
     var cellPading = 10
+
+    var data = {
+        "nodes": [],
+        "combos": []
+    };
+
+    jQuery("canvas").remove();
+    InitMobile((cellHeight + cellFontSize * 3) * gData.length);
+
+
     for (var i in gData) {
         var pdata = gData[i];
         var cellNum = pdata.childs.length;
         var x = 0;
-        const parent = globalData.graph.addNode({
+        var comboName = 'combo_' + i;
+        data.combos.push({
+            id: comboName,
+            type: 'rect',
+            label: ''
+        });
+        for (var j in pdata.childs) {
+            var cdata = pdata.childs[j];
+            data.nodes.push({
+                x: x + cellFontSize / 2,
+                y: y + cellFontSize / 2,
+                id: 'node_' + i + "_" + j,
+                type: 'rect',
+                label: cdata.num,
+                comboId: comboName,
+                style: {
+                    // 仅在 keyShape 上生效
+                    fill: cdata.color,
+                    stroke: '#000',
+                    lineWidth: 1,
+                }
+            });
+            x += cellWidth + cellPading;
+        }
+        y += cellHeight + cellFontSize + cellFontSize;
+
+
+    }
+    console.log(data)
+
+    globalData.graphF6.data(data);
+    globalData.graphF6.render();
+    globalData.graphF6.fitView();
+
+
+}
+
+
+function renderDestop(gData) {
+    var y = 0;
+    var cellWidth = globalData.width / 10;
+    var cellHeight = 60;
+    var cellFontSize = 25
+    var cellPading = 10
+    jQuery("svg").remove();
+
+    globalData.width = $(window).width() * 2 / 3;
+    globalData.graphX6 = new X6.Graph({
+        container: document.getElementById('container-x6'),
+        grid: true,
+        width: globalData.width,
+        height: (cellHeight + cellFontSize * 3) * gData.length,
+        panning: true,
+        embedding: {
+            enabled: true,
+            findParent: function (r) {
+                const node = r.node;
+                const bbox = node.getBBox();
+                return this.getNodes().filter(function (node) {
+                    const data = node.getData();
+                    if (data && data.parent) {
+                        const targetBBox = node.getBBox();
+                        return bbox.isIntersectWithRect(targetBBox)
+                    }
+                    return false
+                })
+            }
+        }
+    });
+
+    globalData.graphX6.clearCells();
+
+    for (var i in gData) {
+        var pdata = gData[i];
+        var cellNum = pdata.childs.length;
+        var x = 0;
+        const parent = globalData.graphX6.addNode({
             x: x,
             y: y,
             width: 6 * (cellWidth + cellPading) + cellFontSize,
@@ -329,7 +432,7 @@ function renderAns(ans) {
         for (var j in pdata.childs) {
             var cdata = pdata.childs[j];
 
-            parent.addChild(globalData.graph.addNode({
+            parent.addChild(globalData.graphX6.addNode({
                 x: x + cellFontSize / 2,
                 y: y + cellFontSize / 2,
                 width: cellWidth,
@@ -353,7 +456,55 @@ function renderAns(ans) {
         y += cellHeight + cellFontSize + cellFontSize;
 
     }
+}
 
+function renderHtml(gData) {
+    $(".card-ans .row").remove();
+    $card = $(".card-ans");
+
+    for (var i in gData) {
+        var pdata = gData[i];
+        var $row = jQuery('<div class="row"></div>');
+        for (var j in pdata.childs) {
+            var cdata = pdata.childs[j];
+            $row.append('<div class="col num select-num col-head ' + cdata.color + '"> <span class="val">' + cdata.num + '</span> </div>');
+        }
+        $card.append($row);
+    }
+
+}
+
+function renderAns(ans) {
+    var gData = []
+    for (var k in ans) {
+        var d = ans[k];
+        var pData = {
+            "childs": []
+        }
+        gData.push(pData);
+
+        if (d.type == "num") {
+            for (var j in d.value) {
+                if (d.value[j] == 0) continue;
+                pData.childs.push({
+                    "num": d.num + 1,
+                    "color": colorName[j]
+                });
+            }
+        } else {
+            var $row = jQuery('<div class="row"></div>');
+            for (var j in d.value) {
+                pData.childs.push({
+                    "num": d.value[j] + 1,
+                    "color": d.color
+                });
+            }
+        }
+    }
+    console.log(gData)
+    renderHtml(gData);
+    renderMobie(gData);
+    renderDestop(gData);
 }
 function Check() {
     var colorNums = []
@@ -364,11 +515,38 @@ function Check() {
         }
         colorNums.push(nums);
     }
+    console.log("colorNums", colorNums);
     var ans = []
     if (DfsCheck(0, colorNums, ans)) {
-        console.log(ans);
+        console.log("ans", ans);
         renderAns(ans);
     } else {
         alert("No");
     }
+}
+
+function isMobile() {
+    var userAgentInfo = navigator.userAgent;
+
+    var mobileAgents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+
+    var mobile_flag = false;
+
+    //根据userAgent判断是否是手机
+    for (var v = 0; v < mobileAgents.length; v++) {
+        if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+            mobile_flag = true;
+            break;
+        }
+    }
+
+    var screen_width = window.screen.width;
+    var screen_height = window.screen.height;
+
+    //根据屏幕分辨率判断是否是手机
+    if (screen_width < 500 && screen_height < 800) {
+        mobile_flag = true;
+    }
+
+    return mobile_flag;
 }
